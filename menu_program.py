e53b9f074b1f171c5878e8e88f482df60bf5d9fe
sfreakinr <subinrajwwe@gmail.com>
2022-02-28 13:12:55 +0530

    Docker completed

--- menu_program.py
+++ menu_program.py
@@ -27,34 +27,40 @@ def Docker():
 	while(1):
 		os.system("tput setaf 6")
 		print("*".center(150,"*"))
-		print("\n1.Docker Installation \n2.Docker Search \n3.Docker Pull\n4.Show Docker Images\n5.Show All Docker Containers\n6.Launch A Docker Container\n7.Start Docker Container\n8.Stop Docker Container\n9.Return to main menu")
+		print("\n1.Docker Installation \n2.Docker Search \n3.Docker Pull\n4.Show Docker Images\n5.Show All Docker Containers\n6.Launch A Docker Container\n7.Start Docker Container\n8.Stop Docker Container\n9.Remove A Docker Container\n10.Return to main menu")
 		sub_choice=input("\nEnter Your Choice : ")
 		if sub_choice=='1':
 			os.system("yum install docker-ce --nobest -y")
 			print("Docker sucessfully installed....")
-		if sub_choice=='2':
+		elif sub_choice=='2':
 			img=input("Enter The Image Name To Search : ")
 			os.system("docker search "+img)
-		if sub_choice=='3':
+		elif sub_choice=='3':
 			img=input("Enter The Image Name You Want To Pull : ")
 			os.system("docker pull "+img)
-		if sub_choice=='4':
+		elif sub_choice=='4':
 			os.system("docker images")
-		if sub_choice=='5':
+		elif sub_choice=='5':
 			os.system("docker ps -a")
-		if sub_choice=='6':
+		elif sub_choice=='6':
 			con_name=input("Enter Name For Your Container : ")
 			img=input("Enter Image Name : ")
 			os.system("tput setaf 3")
 			os.system("docker run -dit --name {} {}".format(con_name, img))
-		if sub_choice=='7':			
+		elif sub_choice=='7':			
 			img=input("Enter Image Name : ")
 			os.system("docker start "+img)
-		if sub_choice=='8':			
-			img=input("Enter Image Name : ")
+		elif sub_choice=='8':			
+			img=input("Enter Container Name : ")
 			os.system("docker stop "+img)
-		if sub_choice=='9':
+		elif sub_choice=='9':
+			img=input("Enter Container Name : ")
+			os.system("docker rm -f "+img)
+		elif sub_choice=='10':
 			break;
+		else:
+			print("Oops!!! Wrong Choice, Please Try Again")
+			os.system("tput setaf 2")	
 		
 
 os.system("tput setaf 3")
@@ -64,7 +70,7 @@ welcome="Linux Menu Program Using Python"
 print(welcome)
 while True:
 	os.system("tput setaf 3")
-	print(" \n 1.Basic Linux Commands \n 2.Docker \n 3.Apache httpd(WebServer) \n 4.Exit \n")
+	print(" \n 1.Basic Linux Commands \n 2.Docker \n 3.Apache HTTPD \n 4.Exit \n")
 	print("-".center(150,"*"))
 	main_choice=input("Choose your domain(1-4):")
 	if main_choice=='4':
@@ -82,7 +88,9 @@ while True:
 			Docker()
 			break;
 		else:
-			print("Wrong command!!Please try again")
+			os.system("tput setaf 6")
+			print("\n\nWrong command!!Please try again\n\n")
+			break;
 				
 		
 	else:
